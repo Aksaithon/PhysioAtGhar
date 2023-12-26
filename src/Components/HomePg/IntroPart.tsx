162c26d103056aa8a7a3289afb9f0eeab8ebc454
Aksaithon <75464693+Aksaithon@users.noreply.github.com>
2023-12-26 22:08:53 +0530

    trial in IntroPart

--- src/Components/HomePg/IntroPart.tsx
+++ src/Components/HomePg/IntroPart.tsx
@@ -1,14 +1,23 @@
 // import React from 'react'
-import './IntroPart.css'
+import "./IntroPart.css";
 
 const IntroPart = () => {
   return (
-    <header className='intro_part' >
-        <p className='main_headline' >Path to recovery starts at home.</p>
-        <button className='contact_btn'>Contact me</button>
-        <button className='book_btn'>Book session</button>
+    <header
+      className="intro_part"
+      style={{
+        background:
+          "linear-gradient(0deg,rgba(0, 0, 0, 0.73) 0%,rgba(0, 0, 0, 0.73) 100%),url(corrected-bg-image.png),lightgray 50% / cover no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <p className="main_headline">Path to recovery starts at home.</p>
+      <button className="contact_btn">Contact me</button>
+      <button className="book_btn">Book session</button>
     </header>
-  )
-}
+  );
+};
 
-export default IntroPart
+export default IntroPart;
